749268c2f0fd3b2ea11ba706528739be9d3c21b4
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-06-09 23:47:21 +0530

    store encryption iv in base64 format in encryted text

--- src/tools/encrypt_decrypt.server.ts
+++ src/tools/encrypt_decrypt.server.ts
@@ -4,7 +4,6 @@ import { hash_sha3_256 } from '@tools/hash.server';
 const ALGORITHM = 'aes-256-cbc';
 const OUTPUT_FORMAT = 'base64';
 const IV_LENGTH = 16;
-const IV_PREPEND_FORMAT = 'hex';
 
 function generateKeyBuffer(key: string) {
   // returns a buffer of length 32 and hex of length 64 so sha3-256 suitable
@@ -18,18 +17,18 @@ function generateKeyBuffer(key: string) {
 export function encrypt_text(text: string, key: string) {
   // iv :- initialization vector for random output
   const iv = crypto.randomBytes(IV_LENGTH);
-  const iv_hex = iv.toString(IV_PREPEND_FORMAT);
+  const iv_base64 = iv.toString('base64');
   const key_buffer = generateKeyBuffer(key);
   const cipher = crypto.createCipheriv(ALGORITHM, key_buffer, iv);
   let encrypted = cipher.update(text, 'utf8', OUTPUT_FORMAT);
   encrypted += cipher.final(OUTPUT_FORMAT);
-  return iv_hex + encrypted;
+  return `${iv_base64}-${encrypted}`;
 }
 
 export function decrypt_text(encrypted: string, key: string) {
   const key_buffer = generateKeyBuffer(key);
-  const iv = Buffer.from(encrypted.slice(0, IV_LENGTH * 2), IV_PREPEND_FORMAT);
-  const encryptedText = encrypted.slice(IV_LENGTH * 2);
+  const [iv_base64, encryptedText] = encrypted.split('-');
+  const iv = Buffer.from(iv_base64, 'base64');
   const decipher = crypto.createDecipheriv(ALGORITHM, key_buffer, iv);
   let decrypted = decipher.update(encryptedText, OUTPUT_FORMAT, 'utf8');
   decrypted += decipher.final('utf8');
